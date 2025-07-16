ac10e143a4c68b31834df1840344eab580b10216
ram1017 <89736374+ram1017@users.noreply.github.com>
2025-07-16 12:18:13 +0530

    Delete book-list/src/app/providers.tsx

--- book-list/src/app/providers.tsx
+++
@@ -1,9 +0,0 @@
-
-"use client";
-
-import { ApolloProvider } from "@apollo/client";
-import client from "@/lib/apolloClient"; 
-
-export function Providers({ children }: { children: React.ReactNode }) {
-  return <ApolloProvider client={client}>{children}</ApolloProvider>;
-}
